d21f4b33092f58ef6ea26511a398bde075646b9d
suelendias2 <165045573+suelendias2@users.noreply.github.com>
2024-09-03 10:33:14 -0300

    Add files via upload

---
+++ aula1/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// este é o primeiro exemplo do curso de linguagem c
+
+int main(void) {
+  // exibi as informações no console
+  printf("meu primeiro trabalho em c\n");
+  printf("meu nome é: suelen");
+  // sucesso ao compilar o codigo
+  return 0;
+}
